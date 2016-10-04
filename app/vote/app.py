5a7242e319e9e5f232bd497ff4d20246c1b939cf
Laurent Bernaille <laurent.bernaille@gmail.com>
2016-10-04 16:09:14 +0200

    New title

--- app/vote/app.py
+++ app/vote/app.py
@@ -107,8 +107,8 @@ def hello():
     score_a = str(int(score_a))
     score_b = str(int(score_b))
 
-    title = option_a + " vs " + option_b
-    # title = "Hello TIAD"
+    #title = option_a + " vs " + option_b
+    title = "Hello TIAD"
     #title=get_param("title",color,"Hello TIAD")
 
     message = "Served by stack " + color
