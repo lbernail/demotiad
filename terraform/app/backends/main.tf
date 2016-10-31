352a0a60ecb076254e6583332ac4382ecde26157
Laurent Bernaille <laurent.bernaille@gmail.com>
2016-10-31 13:39:10 +0100

    Use a separate tf file for redis task

--- terraform/app/backends/main.tf
+++ terraform/app/backends/main.tf
@@ -14,16 +14,6 @@ variable "vpc_state_key" {
   type = "string"
 }
 
-variable "bridge_ip" {
-  type = "string"
-  default = "172.17.0.1"
-}
-
-variable "redis_volume_name" {
-  type = "string"
-  default = "redis"
-}
-
 provider "aws" {
   region = "${var.region}"
 }
@@ -49,32 +39,3 @@ data "terraform_remote_state" "vpc" {
     region = "${var.region}"
   }
 }
-
-data "template_file" "redis" {
-  template = "${file("${path.module}/files/redis.tpl.json")}"
-
-  vars {
-      TF_REGION = "${var.region}"
-      TF_LOG_GROUP = "${data.terraform_remote_state.ecs.log_group}"
-      TF_REDIS_VOLUME = "${var.redis_volume_name}"
-  }
-}
-
-resource "aws_ecs_task_definition" "redis" {
-  family = "redis"
-  container_definitions = "${data.template_file.redis.rendered}"
-
-  volume {
-    name = "${var.redis_volume_name}"
-    host_path = "${data.terraform_remote_state.ecs.efs_mount_point}/redis"
-  }
-
-}
-
-resource "aws_ecs_service" "redis" {
-  name = "redis"
-  cluster = "${data.terraform_remote_state.ecs.cluster}"
-  task_definition = "${aws_ecs_task_definition.redis.arn}"
-  desired_count = 1
-}
-
